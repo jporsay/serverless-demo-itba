b9269a1922169ec6da87c93819d65eb70d2b33bf
Qcho <qcho86@gmail.com>
2018-06-02 15:25:48 -0300

    save database record

--- functions/src/generateThumbnail.ts
+++ functions/src/generateThumbnail.ts
@@ -1,3 +1,4 @@
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import * as path from 'path';
 import * as sharp from 'sharp';
@@ -7,6 +8,16 @@ admin.initializeApp();
 
 const THUMB_MAX_WIDTH = 400;
 const THUMB_MAX_HEIGHT = 400;
+
+admin.initializeApp();
+const firestore = admin.firestore();
+
+interface Image {
+    userIdUploader: string,
+    imagePath: string,
+    thumbPath: string,
+    uploadTime: Date,
+}
 
 export default functions.storage.object().onFinalize((object) => {
     const fileBucket = object.bucket;
@@ -44,6 +55,12 @@ export default functions.storage.object().onFinalize((object) => {
 
     return streamAsPromise.then(() => {
         console.log('Thumbnail created successfully');
+        firestore.collection('images').add({
+            userIdUploader: filePath.match('.*/images/(\\w+)/.*')[1],
+            imagePath: filePath,
+            thumbPath: thumbFilePath,
+            uploadTime: new Date(),
+        } as Image)
         return null;
     });
 });
